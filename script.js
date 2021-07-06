8cae173ca092142047713dfae5a14e5d001ea703
natasaghinadze <85290787+natasaghinadze@users.noreply.github.com>
2021-07-06 11:26:03 -0700

    3-4-davaleba

---
+++ script.js
@@ -0,0 +1,12 @@
+// 3. შექმენით ობიექტი და ციკლის საშუალებით დაბეჭდეთ key-იები;
+let names = ['tornike', 'nata', 'lile', 'lasha'];
+for (let item in names) {
+    console.log (item);
+}
+
+// 4. შექმენით ობიექტი და ციკლის საშუალებით დაბეჭდეთ მნიშვნელობები
+
+let x = ['tornike', 'nata', 'lile', 'lasha'];
+ for(let item of x) {
+     console.log (item);
+ }
